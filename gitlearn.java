ad3d2de8cbd9551f987f13fa044fab41700de40b
Shibin Vettem Veettil <53273782+shibinnarayanan@users.noreply.github.com>
2023-02-23 22:00:26 +0000

    Update gitlearn.java

--- gitlearn.java
+++ gitlearn.java
@@ -4,6 +4,7 @@ public class Test()
       public static void main()
       {
           System.out.println("Hi");
+            System.out.println("first changes at remote repo after commit change in local repo");
       }
 
 }
